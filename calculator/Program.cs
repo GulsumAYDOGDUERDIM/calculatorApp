5383ae7d7d8c317da744715fca29b1c3e55f980b
GulsumAYDOGDUERDIM <gulsumaydogduerdim@gmail.com>
2023-03-26 18:24:39 +0300

    feat add title fix

---
+++ calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hesap Makinası");
+
+        }
+    }
+}
