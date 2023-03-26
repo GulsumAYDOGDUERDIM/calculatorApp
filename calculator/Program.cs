5416e19a04de10951a963f42641f901b9e634b26
GulsumAYDOGDUERDIM <gulsumaydogduerdim@gmail.com>
2023-03-26 18:28:18 +0300

    feat add user inputs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinası");
+            Console.Write("BİRİNCİ SAYIYI GİRİNİZ: ");
+            int sayi1=int.Parse(Console.ReadLine());
+
+            Console.Write("ikinci sayıyı giriniz: ");
+            int sayi2=int.Parse(Console.ReadLine());
 
         }
     }
