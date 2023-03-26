5d5948059f653649be7a9050e5ee14bc70ffcbbc
GulsumAYDOGDUERDIM <gulsumaydogduerdim@gmail.com>
2023-03-26 18:45:35 +0300

    feat: add sum function

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,8 +8,21 @@
             Console.Write("BİRİNCİ SAYIYI GİRİNİZ: ");
             int sayi1=int.Parse(Console.ReadLine());
 
+            Console.Write("Lütfen bir işlem seçiniz(+) (-) (*) (/) (%) :  ");
+            string islem = Console.ReadLine();
+
             Console.Write("ikinci sayıyı giriniz: ");
             int sayi2=int.Parse(Console.ReadLine());
+            switch (islem)
+            {
+                case "+":
+                    Console.WriteLine( sayi1+sayi2);
+                    break;
+            }
+
+
+
+
 
         }
     }
